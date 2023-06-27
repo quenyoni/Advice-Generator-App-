7f3797d07ed6a674f25ab67a6d55ee29d5ee7621
Qobo <quenyoni@gmail.com>
2023-06-27 22:15:16 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
+import {useState,useEffect} from 'react'
+import dice from './assets/images/icon-dice.svg'
+import axios from "axios";
+import splitter from './assets/images/pattern-divider-desktop.svg'
 import './App.css';
 
+
 function App() {
+  
+  
+  
+  
+const [advice, setAdvice] = useState({id:1, advice:'Try Again'})
+    
+  const fetchData = async () => {
+  
+
+  try {
+    let response = await axios.get(`https://api.adviceslip.com/advice/${Math.floor(Math.random()*225)}`)
+  
+ setAdvice(response.data.slip)
+
+    
+    
+    // console.log(advice.slip.id)
+  }
+
+      catch (error) {
+        console.error(error.message);
+     
+  
+  }
+    
+
+    };
+
+
+
+
+  
+    useEffect(() => {fetchData()}, [])
+    
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+      <div className='container'>
+        
+      <div >
+
+           <div>
+             <span className='advice-num'>ADVICE #{advice? advice.id  :'UH OH'}</span>
+           </div>
+          <div>
+             <blockquote className={`quote fade-in `}
+             >
+               "{advice? advice.advice  :'UH OH'}"
+             </blockquote>
+           </div> 
+           <img className='splitter-icon' src={splitter} alt="" />
+           <button onClick={fetchData} className='dice-wrapper'>
+             <img className='dice' src={dice} alt="" />
+           </button>
+        
+         </div>
+
+
+        
+        
+      </div>
+  </div>
+
+
+  )
+
+
+  
+
+
 }
 
+
+//    {
+//      if(advice) {
+//        return (
+//          (
+        
+//          )
+//        )
+//      }
+//      else {
+
+//        return (
+//          (<div>
+
+//            <div>
+//              <span className='advice-num'>UH OH!</span>
+//            </div>
+//            <div>
+//              <blockquote className='quote'>
+//                "Try Again Later"
+               
+//              </blockquote>
+//            </div>
+//            <img className='splitter-icon' src={splitter} alt="" />
+//            <button onClick={useFetchData} className='dice-wrapper'>
+//              <img className='dice' src={dice} alt="" />
+        
+//            </button>
+        
+//          </div>
+//          ))
+//      }
+//    }
+
+//    </div>
+//    </div>
+//  }
+  
+
+
+
+
+
+
 export default App;
+
+
+
+
+
